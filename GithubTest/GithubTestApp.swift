79479e926ede7b31316dcf7a4c34d1c2479840f6
codenameabel <newabelmartinez@gmail.com>
2022-06-20 10:15:46 -0700

    Initial Commit

---
+++ GithubTest/GithubTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  GithubTestApp.swift
+//  GithubTest
+//
+//  Created by Abel Martinez on 6/20/22.
+//
+
+import SwiftUI
+
+@main
+struct GithubTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
